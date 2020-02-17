11eb267e401087b56c4a24a6c9a7e8b3dc944e8c
DusanMijailovic <61167469+DusanMijailovic@users.noreply.github.com>
2020-02-18 00:41:53 +0100

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,3 @@
-// window.onload = function() {
-// 	document.querySelector("#loginBtn").addEventListener("click", login);
-// }
-
-
 function login() {
 	var email = document.querySelector('#email').value;
 	var password = document.querySelector('#password').value;
@@ -47,43 +42,3 @@ function login() {
 		return false;
 	}
 }
-// 		emailError.textContent = "";
-// 		passwordError.textContent = "";
-// 		$.ajax({
-// 			// url: "php/login.php",
-// 			method : "POST",
-// 			data: {
-// 				loginBtn : "send",
-// 				email, password
-// 			},
-// 			dataType: "json",
-// 			success: function (data) {
-// 				console.log(data);
-// 			},
-// 			error : function (xhr,status, err) {
-// 				var alertDiv = document.querySelector('#successMessage');
-// 				var message = document.querySelector('#msg');
-// 				switch (xhr.status) {
-// 					case 409:
-// 						alertDiv.classList.remove('invisible');
-// 						alertDiv.classList.add('alert-warning');
-// 						message.textContent = 'Ne postoji korisnik sa tim email-om/lozinkom. Pokušajte ponovo!';
-// 						break;
-// 					case 422:
-// 						alertDiv.classList.remove('invisible');
-// 						alertDiv.classList.add('alert-warning');
-// 						message.textContent = 'Greške!';
-// 						break;
-// 				}
-// 			}
-// 		})
-// 	} else {
-// 		console.log("Ima gresaka");
-
-
-
-
-	
-
-
-
